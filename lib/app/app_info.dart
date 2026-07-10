5592e6ace6f5dd3423066de61b17c6527bf1404f
Ian Mbae <Stewiegriffin3108ia@gmail.com>
2026-07-10 10:03:53 +0300

    Bump release to 1.1.3 (11) and remove unused UI widgets.

--- lib/app/app_info.dart
+++ lib/app/app_info.dart
@@ -1,7 +1,7 @@
 /// Keep in sync with pubspec.yaml version.
 class AppInfo {
   static const String name = 'QR Vault';
-  static const String version = '1.1.2';
-  static const int buildNumber = 10;
+  static const String version = '1.1.3';
+  static const int buildNumber = 11;
   static const String versionLabel = '$version ($buildNumber)';
 }
